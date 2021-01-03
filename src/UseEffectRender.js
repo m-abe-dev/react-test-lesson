0d7b9cb9aa5af3cc968a9b4f026e409debcbce94
masashiabe <tech.abe.jp@gmail.com>
2021-01-03 15:44:03 +0900

    useeffect rendering test

--- src/UseEffectRender.js
+++ src/UseEffectRender.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import UseEffectRender from "./UseEffectRender";
+import axios from "axios";
+const UseEffectRender = () => {
+  const [user, setUser] = React.useState(null);
+  const fetchJSON = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+    return res.data;
+  };
+  React.useEffect(() => {
+    const fetchUser = async () => {
+      const user = await fetchJSON();
+      setUser(user);
+    };
+    fetchUser();
+  }, []);
+  return (
+    <div>
+      {user ? (
+        <p>
+          I am {user.username} : {user.email}
+        </p>
+      ) : null}
+    </div>
+  );
+};
 
-describe("useEffect rendering", () => {
-  it("Should render only after async function resolved", async () => {
-    render(<UseEffectRender />);
-    expect(screen.queryByText(/I am/)).toBeNull();
-    expect(await screen.findByText(/I am/)).toBeInTheDocument();
-  });
-});
+export default UseEffectRender;
--- src/UseEffectRender.test.js
+++ src/UseEffectRender.test.js
@@ -1,27 +1,11 @@
 import React from "react";
-import axios from "axios";
-const UseEffectRender = () => {
-  const [user, setUser] = React.useState(null);
-  const fetchJSON = async () => {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/users/1");
-    return res.data;
-  };
-  React.useEffect(() => {
-    const fetchUser = async () => {
-      const user = await fetchJSON();
-      setUser(user);
-    };
-    fetchUser();
-  }, []);
-  return (
-    <div>
-      {user ? (
-        <p>
-          I am {user.username} : {user.email}
-        </p>
-      ) : null}
-    </div>
-  );
-};
+import { render, screen } from "@testing-library/react";
+import UseEffectRender from "./UseEffectRender";
 
-export default UseEffectRender;
+describe("useEffect rendering", () => {
+  it("Should render only after async function resolved", async () => {
+    render(<UseEffectRender />);
+    expect(screen.queryByText(/I am/)).toBeNull();
+    expect(await screen.findByText(/I am/)).toBeInTheDocument();
+  });
+});
